a1bedc9fd854cb8c490c869fdbb3b8808a019c72
Stanislav <stas909@gmail.com>
2016-05-21 21:52:46 +0300

    Blank project

---
+++ src/main/java/test/TestClass.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by GreenNun on 21.05.2016.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Blank Project");
+    }
+}
