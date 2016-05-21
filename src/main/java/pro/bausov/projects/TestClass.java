0dec9e13704f644633f34d71d0fcf5de5501dd72
Stanislav <stas909@gmail.com>
2016-05-21 22:13:03 +0300

    Blank project

--- src/main/java/pro/bausov/projects/TestClass.java
+++ src/main/java/pro/bausov/projects/TestClass.java
@@ -1,10 +1,10 @@
-package test;
+package pro.bausov.projects;
 
 /**
  * Created by Stanislav Bausov on 21.05.2016.
  */
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Blank Project");
+        System.out.println("Test");
     }
 }
